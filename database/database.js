c1653cb0d11ae07924a9ad17e0a4b11ea34b69aa
ludovicspina <ludovic.spina@proton.me>
2025-02-18 01:19:42 +0100

    Update

--- database/database.js
+++ database/database.js
@@ -1,16 +1,14 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    logging: false, // désactive les logs SQL (optionnel)
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false, // nécessaire pour Heroku
-        },
-    },
+const { DATABASE_NAME } = process.env.DATABASE_NAME;
+const { DATABASE_PASSWORD } = process.env.DATABASE_PASSWORD;
+const { DATABASE_USER } = process.env.DATABASE_USER;
+
+const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, {
+    host: '192.168.1.116',  // Votre Raspberry Pi
+    dialect: 'mysql',
+    logging: false,  // Désactive les logs SQL
 });
 
 module.exports = sequelize;
