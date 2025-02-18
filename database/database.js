3533ba27c7b9a7b392077cb8129ea147c1e1d42f
ludovicspina <ludovic.spina@proton.me>
2025-02-18 23:23:34 +0100

    database fix

--- database/database.js
+++ database/database.js
@@ -6,7 +6,7 @@ const { DATABASE_PASSWORD } = process.env.DATABASE_PASSWORD;
 const { DATABASE_USER } = process.env.DATABASE_USER;
 
 const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, {
-    host: '192.168.1.116',  // Votre Raspberry Pi
+    host: 'localhost',  // Votre Raspberry Pi
     dialect: 'mysql',
     logging: false,  // Désactive les logs SQL
 });
